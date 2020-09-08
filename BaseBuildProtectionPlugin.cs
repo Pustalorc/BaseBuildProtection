993b14430e771b408e369117434eac1af81d8018
Vicente Pastor <persiafighter@gmail.com>
2020-09-08 16:56:41 +0200

    Add a patch to support decay

--- BaseBuildProtectionPlugin.cs
+++ BaseBuildProtectionPlugin.cs
@@ -1,9 +1,15 @@
-﻿using Pustalorc.Plugins.BaseClustering;
+﻿
+using Pustalorc.Plugins.BaseClustering;
 using Pustalorc.Plugins.BaseClustering.API.Statics;
+using Rocket.API;
 using Rocket.Core.Plugins;
 using SDG.Unturned;
 using Steamworks;
+using System.Linq;
 using UnityEngine;
+#if DecayPatch
+using Pustalorc.ImperialPlugins.Decay;
+#endif
 
 namespace Pustalorc.Plugins.BaseBuildProtection
 {
@@ -29,40 +35,64 @@ namespace Pustalorc.Plugins.BaseBuildProtection
             ref float angleX, ref float angleY, ref float angleZ, ref ulong owner, ref ulong group,
             ref bool shouldAllow)
         {
-            if (BaseClusteringPlugin.Instance == null)
-            {
-                Logging.Write(this, "Base Clustering is not loaded. Will not perform any more code.");
-                return;
-            }
-
-            var bestCluster =
-                BaseClusteringPlugin.Instance.Clusters.FindBestCluster(point,
-                    Configuration.Instance.ExtraProtectionRadius);
-
-            if (bestCluster == null) return;
-
-            shouldAllow = bestCluster.CommonOwner == owner ||
-                          group != CSteamID.Nil.m_SteamID && group == bestCluster.CommonGroup;
+            shouldAllow = CheckValidDeployPosAndOwner(point, owner, group);
         }
 
         private void OnBarricadeDeploy(Barricade barricade, ItemBarricadeAsset asset, Transform hit, ref Vector3 point,
             ref float angleX, ref float angleY, ref float angleZ, ref ulong owner, ref ulong group,
             ref bool shouldAllow)
         {
+            shouldAllow = CheckValidDeployPosAndOwner(point, owner, group);
+        }
+
+        private bool CheckValidDeployPosAndOwner(Vector3 point, ulong owner, ulong group)
+        {
             if (BaseClusteringPlugin.Instance == null)
             {
                 Logging.Write(this, "Base Clustering is not loaded. Will not perform any more code.");
-                return;
+                return true;
             }
 
-            var bestCluster =
-                BaseClusteringPlugin.Instance.Clusters.FindBestCluster(point,
-                    Configuration.Instance.ExtraProtectionRadius);
+            var bestCluster = BaseClusteringPlugin.Instance.Clusters.FindBestCluster(point, Configuration.Instance.ExtraProtectionRadius);
 
-            if (bestCluster == null) return;
+            if (bestCluster == null) return true;
 
-            shouldAllow = bestCluster.CommonOwner == owner ||
-                          group != CSteamID.Nil.m_SteamID && group == bestCluster.CommonGroup;
+            var commonOwner = bestCluster.CommonOwner;
+            var commonGroup = bestCluster.CommonGroup;
+
+#if DecayPatch
+            var config = AdvancedDecayPlugin.Instance.Configuration.Instance.Decays.LastOrDefault(k =>
+                new RocketPlayer(commonOwner.ToString()).HasPermission(k.Permission));
+            var tcList = AdvancedDecayPlugin.Instance.defaultRustConfiguration.Instance.ToolCupboardItemIds;
+
+            if (config != null)
+                tcList.AddRange(config.RustDecaySettings.ToolCupboardIds);
+
+            var baseTcs = bestCluster.Buildables.Where(l => tcList.Contains(l.AssetId));
+
+            var changedOwner = false;
+            do
+            {
+                if (!baseTcs.Any()) break;
+
+                var newOwner = baseTcs.Where(l => l.Owner != CSteamID.Nil.m_SteamID).GroupBy(l => l.Owner)
+                    .OrderByDescending(l => l.Count()).Select(g => g.Key).FirstOrDefault();
+                changedOwner = newOwner != commonOwner;
+                commonOwner = newOwner;
+
+                commonGroup = baseTcs.Where(l => l.Group != CSteamID.Nil.m_SteamID).GroupBy(l => l.Group)
+                    .OrderByDescending(l => l.Count()).Select(g => g.Key).FirstOrDefault();
+                config = AdvancedDecayPlugin.Instance.Configuration.Instance.Decays.LastOrDefault(k =>
+                    new RocketPlayer(commonOwner.ToString()).HasPermission(k.Permission));
+
+                if (config != null)
+                    tcList.AddRange(config.RustDecaySettings.ToolCupboardIds);
+
+                baseTcs = bestCluster.Buildables.Where(l => tcList.Contains(l.AssetId));
+            } while (changedOwner);
+#endif
+
+            return commonOwner == owner || (group != CSteamID.Nil.m_SteamID && group == commonGroup);
         }
     }
 }
