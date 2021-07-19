1299fd22934798ed9396e7960b73c5f85ac6a8cd
Pustalorc <persiafighter@gmail.com>
2021-07-19 19:00:44 +0200

    Up version number.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("aad899c8-26d0-4c96-9997-c1d7248ed4b6")]
-[assembly: AssemblyVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5")]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6")]
